020809b835bb513b10da8f214b85fc551ab2ca8f
KhanhDu26012002 <93703715+KhanhDu26012002@users.noreply.github.com>
2021-11-04 15:16:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Bỏ Pu đi';
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'nguyen.jpg';
+const giftImage = 'hot-gitl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
