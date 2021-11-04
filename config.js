659fc87016746af96fc2f93d2b0cffd5813e84ff
KhanhDu26012002 <93703715+KhanhDu26012002@users.noreply.github.com>
2021-11-04 15:11:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Em';
+const nameGirl = 'Huỳnh Tuấn NGuyên';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Happy bỉth day';
+const titleCard = 'Huynh Tuấn Nguyên';
+const contentCard = 'Bỏ Pu đi';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'nguyen.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
