b337c5ae66c8a8b063f4dc497653174dd71f4b0d
Trần Khánh Dư <93703715+KhanhDu26012002@users.noreply.github.com>
2021-11-18 19:42:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Cô Xinh Đẹp';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Happy wedding';
+const eventName = 'Happy birthday';
 const titleCard = 'Nguyễn Thị Cẩm Nhung';
-const contentCard = 'Chúc cô trăm năm hạnh phúc';
+const contentCard = 'Chúc cô có nhiều sức khỏe, xinh đẹp đặc biệt là sớm có thiệp mừng( mấy bn chờ lâu lắm r), chúc cô kiếm được nhiều tiền để bao e ăn';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
